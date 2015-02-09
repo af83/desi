8eea870dc05985bd7c0c82ff515b4e9f495ce06c
Dominique Rose-Rosette <drose-rosette@af83.com>
2015-02-09 12:48:12 +0100

    Move LocalInstall::Release to own file
    Signed-off-by: Dominique Rose-Rosette <drose-rosette@af83.com>

--- lib/desi/local_install.rb
+++ lib/desi/local_install.rb
@@ -1,70 +1,9 @@
 # encoding: utf-8
 
-require "pathname"
-require "semantic"
+require_relative "local_install/release"
 
 module Desi
   class LocalInstall
-
-    class Release
-      def self.all_in(workdir)
-        Dir[workdir.join('*')].
-          select {|subdir| File.directory?(subdir) && File.basename(subdir) =~ /^elasticsearch\-\d+\.\d+\.\d+/ }.
-          map {|dirname| new(dirname, workdir) }
-      end
-
-      def initialize(dirname, workdir)
-        @dirname = Pathname.new(dirname)
-        @workdir = workdir
-      end
-
-      def name
-        @name ||= File.basename(@dirname)
-      end
-
-      def current?
-        current_symlink? && current_symlink.realpath == @dirname
-      end
-
-      def version
-        @version ||= Semantic::Version.new(version_number)
-      end
-
-      def to_s
-        current_mark = current? ? '*' : '-'
-
-        " #{current_mark} #{name} (#{@dirname})"
-      end
-
-      def ==(other)
-        other.version.to_s == version.to_s
-      end
-
-      def <=>(other)
-        name <=> other.name
-      end
-
-      def pre_one_zero?
-        @pre_one_zero ||= (version < Semantic::Version.new("1.0.0-alpha"))
-      end
-
-      private
-
-      def current_symlink
-        @current_symlink ||= Pathname(@workdir).join('current')
-      end
-
-      def current_symlink?
-        current_symlink.exist?
-      end
-
-      # Ugly hack to get around elasticsearch's flakey semver naming
-      # (e.g. `1.4.0.Beta1` instead of `1.4.0-beta1`)
-      def version_number
-        /^elasticsearch\-(?<version>.*)$/.match(name.to_s)[:version].
-          sub(/\.(alpha|beta)/i, '-\1')
-      end
-    end # Release
 
     def self.current_release_is_pre_one_zero?
       current = new.current_release
